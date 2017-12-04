6a056a946bae947cc0dcba1bf8bfc73453ce5767
DanielaOlivera <oliverad@berea.edu>
2017-12-04 09:12:07 -0500

    Modified Flower Class so that it collaborates with Class Point

--- Flower_Class.py
+++ Flower_Class.py
@@ -2,16 +2,11 @@
 # Author: Daniela Olivera
 # Username: oliverad
 #
-# Assignment: A3: A Pair of Fully Functional Gitty Psychedelic  Robotic Turtles
+# Assignment: Final Project
 # Purpose: Introduces the use of functions with the turtle library
 ######################################################################
 # Acknowledgements:
-# original from http://openbookproject.net/thinkcs/python/english3e/functions.html
-#
-# Modifications by Dr. Jan Pearce
-# converted to Windows, removed def function, added Screen for clean close
-# licensed under a Creative Commons
-# Attribution-Noncommercial-Share Alike 3.0 United States License.
+# Scott Heggen
 #################################################################################
 import turtle
 
@@ -25,42 +20,52 @@ class Flower:
         The robotic artist needs an inventory to be able to know what type of flower to draw for you
         """
         self.corner = pos          # A Point object to hold the bottom left corner of the rectangle
-        self.petals = num          # Number of petals
+        self.petal_sides = num     # Number of sides each petal has
         self.size = sz             # Length of each side on petal
         self.turtle = turtle.Turtle()
 
-    def triangle_petals(self, sz):
+    def user_set_size(self):
+        self.size = int(input("Enter petal size: [5-20]"))
+
+    def angle_calculator(self):
+        """
+        Calculates, prints and returns angle based on number of sides
+        :return: angle
+        """
+        angle = 360/self.petal_sides
+        print("The number of sides is", self.petal_sides)
+        print("The angle is", angle)
+        return angle
+
+    def triangle_petals(self):
         """
         Make a multi-color triangle
-        :param sz: size of a side of the triangle
         :return: None
         """
         for i in ["black", "silver", "blue"]:
             self.turtle.color(i)
-            self.turtle.forward(sz)
+            self.turtle.forward(self.size)
             self.turtle.left(360/3)
             self.turtle.speed(10)
 
-    def pentagon_petals(self, sz):
+    def pentagon_petals(self):
         """
         Make a multi-color pentagon
-        :param sz: size of a side of the pentagon
         :return: None
         """
         for i in ["black", "white", "purple", "violet", "olive"]:
             self.turtle.color(i)
-            self.turtle.forward(sz)
+            self.turtle.forward(self.size)
             self.turtle.left(360/5)
             self.turtle.speed(10)
 
-    def grow_flower_a(self, size):
+    def grow_flower_a(self):
         """
         Draw a flower on the bottom right side of the screen
-        :param size: size
         :return: None
         """
         self.turtle.penup()
-        self.turtle.setpos(30, -47)
+        self.turtle.goto(self.corner.x, self.corner.y)
 
         for i in range(8):          # Make ella draw 8 semi-circular sets of triangles creating a big circle
             self.turtle.penup()
@@ -68,18 +73,17 @@ class Flower:
             self.turtle.forward(70)
             self.turtle.pendown()
             for i in range(12):     # Make ella draw 12 triangles on each set of triangles
-                self.triangle_petals(size)
+                self.triangle_petals()
                 self.turtle.forward(5)
                 self.turtle.right(15)
 
-    def grow_flower_b(self, size):
+    def grow_flower_b(self):
         """
         Draw a flower on the upper left side of the screen
-        :param size: size
         :return: None
         """
         self.turtle.penup()
-        self.turtle.setpos(-200, 100)
+        self.turtle.goto(self.corner.x, self.corner.y)
         for i in range(9):          # Make dani draw 9 semi-circular sets of pentagons creating a big circle
             self.turtle.speed(10)
             self.turtle.penup()
@@ -87,7 +91,7 @@ class Flower:
             self.turtle.forward(70)
             self.turtle.pendown()
             for i in range(12):     # Make dani draw 12 pentagons on each set of polygons
-                self.pentagon_petals(size)
+                self.pentagon_petals()
                 self.turtle.forward(3)
                 self.turtle.right(13)
 
@@ -100,7 +104,7 @@ class Flower:
         self.turtle.color('#DD6213')
         self.turtle.pensize(5)
         self.turtle.penup()
-        self.turtle.setpos(-40, -165)
+        self.turtle.setpos(-45, -165)
         self.turtle.right(90)
         self.turtle.pendown()
         for i in range(3):     # draw small steam
@@ -128,28 +132,14 @@ class Flower:
     #     shape.setpos(70,120)
     #     shape.pendown()
     #     shape.write(txt,move=False,align='center',font=("Arial", 30, ("bold","normal")))
+# end class
 
-
-def main():
-    """
-    Makes two flowers with geometric patterns.
-    :return: None
-    """
-    wn = turtle.Screen()            # Makes a new turtle screen
-    wn.bgcolor("green")             # Set up screen color
-    #wn.bgpic("giphy.gif")
-    dani = Flower(pos=0, num=3, sz=4)
-    size = 5
-    dani.grow_flower_a(size)
-    dani.grow_flower_b(size)
-    dani.grow_steams()
-    wn.exitonclick()
-
-main()
 # Things to do next:
 # Add circles at center of flower by using even (onclick)
 # make it interactive
-# use point class for coordinate point or maybe onclick?
+# use point class for coordinate point or maybe onclick? OK
 # use background picture of grass or something like that
+# background could change on key
 # add butterflies
 # modify steams so that they have starting and ending position maybe?
+# user draws steams himself?
