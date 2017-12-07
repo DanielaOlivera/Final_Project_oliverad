e832fb5e9ca495f8e0c0a6c89c72577726c21a7c
DanielaOlivera <oliverad@berea.edu>
2017-12-07 03:05:54 -0500

    Modified this program so that it now interacts with the user to ask how many flowers he/she wants and how many sides should each petal have (to draw either triangles in each petal or pentagons). Also added an onkey event to close window when pressing "q".

--- Final_Project_oliverad.py
+++ Final_Project_oliverad.py
@@ -13,25 +13,40 @@ from Flower_Class import Flower
 from Point_Class import Point
 
 
+def h1():
+    """
+    Event handler to close window
+    :return: None
+    """
+    wn.bye()                        # Close down the turtle window
+
+
 def main():
     """
     Makes two flowers with geometric patterns.
     :return: None
     """
+    global wn
     wn = turtle.Screen()            # Makes a new turtle screen
-    wn.bgcolor("green")             # Set up screen color
-    #wn.bgpic("giphy.gif")
-    p = Point(30, -47)
-    dani = Flower(p)
-    dani.user_set_size()
-    dani.grow_flower_a()
-    #dani.grow_flower_b()
-    #dani.grow_steams()
-    p1 = Point(-200, 100)
-    ella = Flower(p1)
-    ella.user_set_size()
-    ella.grow_flower_b()
-    wn.onclick(ella.flower_center)
+    wn.bgpic("grass.gif")
+    p = Point()
+    flowers = int(input("How many flowers dou you want?"))
+
+    for i in range(flowers):
+        global dani
+        p.user_set()
+        dani = Flower(p)
+        dani.user_set_shape_petals()
+        dani.user_set_size()
+        if dani.petal_sides == 3:
+            dani.grow_flower_a()
+        if dani.petal_sides == 5:
+            dani.grow_flower_b()
+
+    wn.onclick(dani.flower_center)
+    wn.onkey(h1, "q")
+    wn.listen()
     wn.mainloop()
+
 
 main()
