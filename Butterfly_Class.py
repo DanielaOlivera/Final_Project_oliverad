72fc39a201ab881b5aaae8df3794ed2bbb19e848
DanielaOlivera <oliverad@berea.edu>
2017-12-07 21:39:01 -0500

    Butterfly now is created in random points

--- Butterfly_Class.py
+++ Butterfly_Class.py
@@ -9,17 +9,18 @@
 # Scott Heggen
 #################################################################################
 import turtle
+import random
 
 
 class Butterfly:
     """
     A butterfly object
     """
-    def __init__(self, pos):
+    def __init__(self):
         """
         Creates flowers at a starting position
         """
-        self.initial_position = pos  # A Point object to hold the starting position
+        #self.initial_position = None  # A Point object to hold the starting position
         self.turtle = turtle.Turtle()
         self.wn = turtle.Screen()
 
@@ -28,17 +29,19 @@ class Butterfly:
         self.turtle.pensize(5)
         self.turtle.color("orange")
         self.turtle.penup()
-        self.turtle.goto(34,90)
+        self.turtle.goto(random.randint(-200, 200), random.randint(-200,200))
         self.turtle.hideturtle()
         self.turtle.pendown()
         self.turtle.begin_fill()
+        a = 8
         for i in range(2):
-            self.turtle.circle(10, 180)
+            self.turtle.circle(a, 180)
             self.turtle.left(-90)
             self.turtle.forward(3)
+            a = 6
         self.turtle.end_fill()
         self.turtle.right(45)
         for h in range(3):
             self.turtle.color("black")# draw body
             self.turtle.forward(-12)
-            self.turtle.right(-3)
+            self.turtle.right(-4)
