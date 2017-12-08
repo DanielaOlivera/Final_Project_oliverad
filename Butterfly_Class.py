0e4c3ee8cb520a93350c79f3217544505e95ec2b
DanielaOlivera <oliverad@berea.edu>
2017-12-08 08:53:21 -0500

    Butterfly is now a picture instead of a drawing

--- Butterfly_Class.py
+++ Butterfly_Class.py
@@ -20,9 +20,9 @@ class Butterfly:
         """
         Creates flowers at a starting position
         """
-        #self.initial_position = None  # A Point object to hold the starting position
         self.turtle = turtle.Turtle()
         self.wn = turtle.Screen()
+        self.wn.addshape("butterfly1.gif")
 
     def draw_butterfly(self):
 
@@ -30,18 +30,5 @@ class Butterfly:
         self.turtle.color("orange")
         self.turtle.penup()
         self.turtle.goto(random.randint(-200, 200), random.randint(-200,200))
-        self.turtle.hideturtle()
-        self.turtle.pendown()
-        self.turtle.begin_fill()
-        a = 8
-        for i in range(2):
-            self.turtle.circle(a, 180)
-            self.turtle.left(-90)
-            self.turtle.forward(3)
-            a = 6
-        self.turtle.end_fill()
-        self.turtle.right(45)
-        for h in range(3):
-            self.turtle.color("black")# draw body
-            self.turtle.forward(-12)
-            self.turtle.right(-4)
+        self.turtle.shape("butterfly1.gif")
+        self.turtle.stamp()
